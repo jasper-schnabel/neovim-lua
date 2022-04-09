11d811939fe71b7b99b91eb0a7685577f9835c0c
jasper-schnabel <jasper.schnabel@arch-m.de>
2022-04-09 15:31:38 +0000

    Update autopairs config

--- plugin/autopairs.lua
+++ plugin/autopairs.lua
@@ -3,13 +3,7 @@ require('nvim-autopairs').setup {
   check_ts = true,
   fast_wrap = {
     map = '<C-w>',
-    chars = { '{', '[', '(', '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
     offset = 0,
     end_key = 'w',
-    keys = 'qwertyuiopzxcvbnmasdfghjkl',
-    check_comma = true,
-    highlight = 'Search',
-    highlight_grey='Comment'
   },
 }
